91eb4db7cb612b5d874577a814d9bfaa44f00d80
hjxyyds <1657158149@qq.com>
2021-09-19 18:28:33 +0800

    'up'

--- router.js
+++ router.js
@@ -3,22 +3,23 @@ var express = require('express')
 const multer = require('multer')
 var fs = require('fs')
 var router = express.Router()
-var User = require('./db.js')
+//var User = require('./db.js')
 router.get('/', function (req, res) {
-    User.findOne({
-        user: '吴灶凯'
-    },function (err, ret) {
-        if (err) {
-            console.log('查询出错');
-        } else {
-            // console.log(ret);
-            res.render('index.html', {
-                user: ret.user,
-                NFTname: ret.NFTname,
-                opus:ret.opus
-            })
-        }
-    })
+    // User.findOne({
+    //     user: '吴灶凯'
+    // },function (err, ret) {
+    //     if (err) {
+    //         console.log('查询出错');
+    //     } else {
+    //         // console.log(ret);
+    //         res.render('index.html', {
+    //             user: ret.user,
+    //             NFTname: ret.NFTname,
+    //             opus:ret.opus
+    //         })
+    //     }
+    // })
+    res.render('index.html');
 })
 router.post('/upload', multer({ dest: 'upload' }).single('file'), (req, res) => {
     console.log(req.file)
